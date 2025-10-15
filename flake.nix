a87a63f9332d784db9c663901d5074f874270e51
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-10-15 16:20:25 -0400

    feat: initial commit, skeleton project

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Node.js development flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {inherit system;};
+      in
+      {
+        # nix develop
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+        };
+      });
+}
