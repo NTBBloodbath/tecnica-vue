bdcccd81087219f154a6004eb1ac9c9ba74d4460
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-10-15 16:49:52 -0400

    feat: add pinia and tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
